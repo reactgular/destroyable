a42c9c008f9a9c8f20809bcf858bb228ce4970fa
codemile <nick@reactgular.com>
2020-09-08 11:03:23 -0400

    improves test quality

--- projects/reactgular/destroyable/src/destroyable.spec.ts
+++ projects/reactgular/destroyable/src/destroyable.spec.ts
@@ -1,58 +1,143 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
-import {forkJoin} from 'rxjs';
+import {Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 import {Destroyable} from './destroyable';
 
+/**
+ * Takes numbers from an observable and stores them in an array for as long as the component is alive.
+ */
 @Component({
   selector: 'rg-destroyable',
   template: ''
 })
-class DestroyableComponent extends Destroyable {
+class DestroyableComponent extends Destroyable implements OnInit, OnDestroy {
+  /**
+   * Extended classes must call super if they implement OnDestroy, but if they do not override the ngOnDestroy then it is not necessary.
+   */
+  public callSuperDestroy: boolean = true;
+
+  /**
+   * True after being destroyed.
+   */
+  public isDestroyed: boolean = false;
+
+  /**
+   * Allows externals to emit values.
+   */
+  public readonly value$: Subject<number> = new Subject<number>();
+
+  /**
+   * A cache of subscribed values.
+   */
+  public values: number[] = [];
+
+  public ngOnDestroy() {
+    if (this.callSuperDestroy) {
+      super.ngOnDestroy();
+    }
+    this.isDestroyed = true;
+  }
+
+  public ngOnInit(): void {
+    this.value$.pipe(
+      takeUntil(this._destroyed$)
+    ).subscribe(value => this.values = [...this.values, value]);
+  }
 }
 
-@Component({template: '<rg-destroyable *ngIf="toggle"></rg-destroyable>'})
-class ExampleComponent {
+@Component({template: '<rg-destroyable *ngIf="create"></rg-destroyable>'})
+class FixtureComponent {
+  public create: boolean = true;
+
   @ViewChild(DestroyableComponent)
-  public destroyableComponent: DestroyableComponent;
+  public rgDestroyable: DestroyableComponent;
 
-  public toggle: boolean = true;
+  public destroyIt() {
+    this.create = false;
+  }
 }
 
-/**
- * @todo add a test for forgetting to call super via destroy (to show it breaks).
- * @todo add a test for catching destroy via takeUntil to show proper usage.
- */
-describe('destroyable', () => {
-  let component: ExampleComponent;
-  let fixture: ComponentFixture<ExampleComponent>;
+describe(Destroyable.name, () => {
+  let fixture: ComponentFixture<FixtureComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         DestroyableComponent,
-        ExampleComponent
+        FixtureComponent
       ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ExampleComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(FixtureComponent);
     fixture.detectChanges();
   });
 
-  it('should emit when the component is destroyed', (done: DoneFn) => {
-    expect(component).toBeTruthy();
-    const destroyable = component.destroyableComponent;
-    expect(destroyable).toBeTruthy();
+  it('should create the component instance', () => {
+    expect(fixture.componentInstance).toBeTruthy();
+  });
 
-    forkJoin([
-      destroyable['_destroyed$'],
-      destroyable.destroyed,
-      destroyable.destroyed$
-    ]).subscribe(() => done());
+  it('should create the destroyable component', () => {
+    expect(fixture.componentInstance.rgDestroyable).toBeTruthy();
+  });
 
-    component.toggle = false;
+  it('should have no emitted numbers when created', () => {
+    expect(fixture.componentInstance.rgDestroyable.values).toEqual([]);
+    expect(fixture.componentInstance.rgDestroyable.isDestroyed).toBeFalsy();
+  });
+
+  it('should cache emitted numbers', () => {
+    const inst = fixture.componentInstance;
+    const destroyable = inst.rgDestroyable;
+
+    expect(destroyable.values).toEqual([]);
+    destroyable.value$.next(1);
+    destroyable.value$.next(2);
+    expect(destroyable.values).toEqual([1, 2]);
+  });
+
+  it('should stop caching emitted numbers after it is destroyed', () => {
+    const inst = fixture.componentInstance;
+    const destroyable = inst.rgDestroyable;
+
+    expect(destroyable.values).toEqual([]);
+    destroyable.value$.next(1);
+    destroyable.value$.next(2);
+    destroyable.value$.next(3);
+    expect(destroyable.values).toEqual([1, 2, 3]);
+
+    inst.destroyIt();
     fixture.detectChanges();
+
+    expect(destroyable.isDestroyed).toBeTruthy();
+
+    destroyable.value$.next(4);
+    destroyable.value$.next(5);
+    destroyable.value$.next(6);
+    expect(destroyable.values).toEqual([1, 2, 3]);
+  });
+
+  it('should not unsubscribe if super.ngOnDestroy() is not called', () => {
+    const inst = fixture.componentInstance;
+    const destroyable = inst.rgDestroyable;
+
+    expect(destroyable.values).toEqual([]);
+    destroyable.value$.next(1);
+    destroyable.value$.next(2);
+    destroyable.value$.next(3);
+    expect(destroyable.values).toEqual([1, 2, 3]);
+
+    destroyable.callSuperDestroy = false;
+    inst.destroyIt();
+    fixture.detectChanges();
+
+    expect(destroyable.isDestroyed).toBeTruthy();
+
+    destroyable.value$.next(4);
+    destroyable.value$.next(5);
+    destroyable.value$.next(6);
+    expect(destroyable.values).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
--- projects/reactgular/destroyable/src/destroyable.ts
+++ projects/reactgular/destroyable/src/destroyable.ts
@@ -1,5 +1,5 @@
 import {Directive, OnDestroy} from '@angular/core';
-import {Observable, Subject} from 'rxjs';
+import {Subject} from 'rxjs';
 
 /**
  * A simple class that emits a destruction event.
@@ -11,22 +11,6 @@ export abstract class Destroyable implements OnDestroy {
    * Emits when the object is destroyed by the Angular framework.
    */
   protected readonly _destroyed$: Subject<void> = new Subject<void>();
-
-  /**
-   * Emits when the object is destroyed by the Angular framework.
-   * @deprecated will be removed in v2
-   */
-  public get destroyed(): Observable<void> {
-    return this._destroyed$.asObservable();
-  }
-
-  /**
-   * Emits when the object is destroyed by the Angular framework.
-   * @deprecated will be removed in v2
-   */
-  public get destroyed$(): Observable<void> {
-    return this._destroyed$.asObservable();
-  }
 
   /**
    * You must call super.ngOnDestroy()
